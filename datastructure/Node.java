778f4febed2fd1507fdb1d4ad09119c17fff6137
Manaia Junior <manaiajr.23@gmail.com>
2019-10-17 17:14:42 -0300

    Create Node.java

---
+++ datastructure/Node.java
@@ -0,0 +1,63 @@
+package datastructure;
+
+import java.util.Map;
+
+public class Node {
+    private int letter;
+    private int count;
+    private Node left;
+    private Node right;
+
+    public Node(int letter) {
+        this.letter = letter;
+    }
+
+    public Node(Node left, Node right) {
+        this.letter = -1;
+        this.left = left;
+        this.right = right;
+    }
+
+    public boolean isLeaf() {
+        return this.left == null && this.right == null;
+    }
+
+    public int getFrequency() {
+        if (this.isLeaf()) {
+            return this.count;
+        }
+
+        return this.left.getFrequency() + this.right.getFrequency();
+    }
+
+    public int getLetter() {
+        return this.letter;
+    }
+
+    public Node getLeft() {
+        return this.left;
+    }
+
+    public Node getRight() {
+        return this.right;
+    }
+
+    public void add() {
+        this.count += 1;
+    }
+
+    public void fillCodingTable(Map<Character, String> codeMap, String edge) {
+        if (this.isLeaf()) {
+            codeMap.put((char) this.getLetter(), edge);
+            return;
+        }
+
+        this.left.fillCodingTable(codeMap, edge + "0");
+        this.right.fillCodingTable(codeMap, edge + "1");
+    }
+
+    @Override
+    public String toString() {
+        return String.format("'%d': %d", this.getLetter(), this.getFrequency());
+    }
+}
