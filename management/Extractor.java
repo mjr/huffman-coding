3a07d3e050a0205330f817605291a01406d8629f
Manaia Junior <manaiajr.23@gmail.com>
2019-10-22 18:21:29 -0300

    Update Extractor.java

--- management/Extractor.java
+++ management/Extractor.java
@@ -6,41 +6,47 @@ import java.util.Map;
 public class Extractor {
     private String text;
     private Map<String, Character> codingTable;
+    private String result = "";
 
     public void extract(String fileCompressedFile, String fileCodingTable, String fileWrite) {
         this.readFileCodingTable(fileCodingTable);
 
         this.readFileCompressedFile(fileCompressedFile);
 
+        this.setResult();
+
         this.writeFile(fileWrite);
     }
 
-    private void readFileCodingTable(String fileCodingTable) {
-        System.out.println("READ " + fileCodingTable);
-        File fileRead = File.read(fileCodingTable);
-        this.createCodingTable(fileRead.getText());
-    }
-
-    private void readFileCompressedFile(String fileCompressedFile) {
-        System.out.println("READ " + fileCompressedFile);
-        File fileCompressedRead = File.read(fileCompressedFile);
-        this.text = fileCompressedRead.getText();
-    }
-
-    private void writeFile(String fileWrite) {
+    public void setResult() {
         String temp = "";
-        String result = "";
         for (int i = 0; i < this.text.length(); i++) {
             temp += this.text.charAt(i);
 
             if (this.codingTable.containsKey(temp)) {
-                result += this.codingTable.get(temp);
+                int tempResult = (int) this.codingTable.get(temp);
+                if (tempResult == 65526) {
+                    return;
+                }
+
+                this.result += this.codingTable.get(temp);
                 temp = "";
             }
         }
+    }
 
-        System.out.println("WRITE " + fileWrite);
-        File.write(fileWrite, result);
+    private void readFileCodingTable(String fileCodingTable) {
+        File fileRead = File.readExtract(fileCodingTable);
+        this.createCodingTable(fileRead.getText());
+    }
+
+    private void readFileCompressedFile(String fileCompressedFile) {
+        File fileCompressedRead = File.readByte(fileCompressedFile);
+        this.text = fileCompressedRead.getText();
+    }
+
+    private void writeFile(String fileWrite) {
+        File.write(fileWrite, this.result.getBytes());
     }
 
     private void createCodingTable(String data) {
@@ -49,7 +55,7 @@ public class Extractor {
         String[] arrOfStr = data.split("\n");
 
         for (String a: arrOfStr) {
-            if (a.contains("-")) {
+            if (a.substring(1).contains("-")) {
                 result.put(a.substring(3), (char) Integer.parseInt(a.substring(0, 2)));
             } else {
                 result.put(a.substring(1), a.charAt(0));
